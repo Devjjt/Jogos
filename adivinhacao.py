d486e9632e785c1640f547868487beddcabbb2fe
Devjjt <jonathanleupoldino@gmail.com>
2023-03-31 09:55:18 -0300

    commit and push

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,12 +3,28 @@ print("Bem vindo no jogo de adivinhação")
 print("********************************")
 
 numero_secreto = 42
+total_de_tentativas = 3
 
-chute = int(input("Digite o seu numero: "))
+for rodada in range(1, total_de_tentativas + 1):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute = int(input("Digite um número entre 1 e 100: "))
+    print("Você digitou: ", chute)
 
-print("Você digitou: ", chute)
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um numero entre 1 e  100!")
+        continue
 
-if(numero_secreto == chute):
-    print("Você acertou")
-else:
-    print("Você errou!")
+    acertou = numero_secreto == chute
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if(acertou):
+        print("Você acertou")
+        break
+    else:
+        if(maior):
+            print("Você errou! O seu chute foi maior do que o número secreto.")
+        elif(menor):
+            print("Você errou! O seu chute foi menor do que o número secreto.")
+
+print("Fim do Jogo")
